c260ace7e64b6d9d3d84c103091e6daecb554588
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-07-12 22:26:44 +0300

    solution - 02. Bus Schedule -> added functionality for auto load names and phones when add new person and delete person

--- 04 REST Services And AJAX/03.PHONEBOOK/app.js
+++ 04 REST Services And AJAX/03.PHONEBOOK/app.js
@@ -1,54 +1,65 @@
 function attachEvents() {
+
     document.querySelector('#btnLoad')
-        .addEventListener('click', function () {
-            const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
-
-            fetch(url)
-                .then((request) => request.json())
-                .then((data) => {
-                    document.getElementById('phonebook').innerHTML = '';
-
-                    const keys = Object.keys(data);
-
-                    for (const key of keys) {
-                        const name = data[key].person;
-                        const phoneNumber = data[key].phone;
-
-                        const dellBtn = document.createElement('button');
-                        dellBtn.textContent = 'Delete';
-                        dellBtn.addEventListener('click', function () {
-                            const url = `https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`;
-                            fetch(url, {
-                                method: 'delete'
-                            })
-                                .then(response => response.json());
-                        });
-
-                        const listItem = document.createElement('li');
-                        listItem.textContent = `${name}: ${phoneNumber}`;
-                        listItem.appendChild(dellBtn);
-
-                        document.getElementById('phonebook').appendChild(listItem);
-                    }
-                });
-        });
+        .addEventListener('click', () => loadNamesAndPhones());
 
     document.querySelector('#btnCreate')
-        .addEventListener('click', function () {
-            const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
-            const person = document.querySelector('#person').value;
-            const phone = document.querySelector('#phone').value;
+        .addEventListener('click', () => deleteNamesAndPhones());
 
-            let obj = {person, phone};
-            fetch(url, {
-                method: 'post',
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify(obj)
+    function loadNamesAndPhones() {
+        const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
+        fetch(url)
+            .then((request) => request.json())
+            .then((data) => {
+                document.getElementById('phonebook').innerHTML = '';
+
+                const keys = Object.keys(data);
+
+                for (const key of keys) {
+                    const name = data[key].person;
+                    const phoneNumber = data[key].phone;
+
+                    const dellBtn = document.createElement('button');
+                    dellBtn.textContent = 'Delete';
+                    dellBtn.addEventListener('click', function () {
+                        const url = `https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`;
+                        fetch(url, {
+                            method: 'delete'
+                        })
+                            .then(response => response.json())
+                            .then(response => {
+                                loadNamesAndPhones();
+                            });
+                    });
+
+                    const listItem = document.createElement('li');
+                    listItem.textContent = `${name}: ${phoneNumber}`;
+                    listItem.appendChild(dellBtn);
+
+                    document.getElementById('phonebook').appendChild(listItem);
+                }
+            });
+    }
+
+    function deleteNamesAndPhones() {
+        const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
+        const person = document.querySelector('#person').value;
+        const phone = document.querySelector('#phone').value;
+
+        let obj = {person, phone};
+        fetch(url, {
+            method: 'post',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(obj)
+        })
+            .then(response => response.json())
+            .then(response => {
+                loadNamesAndPhones();
             });
 
-            document.querySelector('#person').value = '';
-            document.querySelector('#phone').value = '';
-        });
+        document.querySelector('#person').value = '';
+        document.querySelector('#phone').value = '';
+    }
 }
 
 attachEvents();
