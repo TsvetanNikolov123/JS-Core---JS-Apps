fffa6f49b57c92729e6fc1c52082a1853b4b0f37
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-07-15 22:47:23 +0300

    solution - 01. Github-Commits

--- 05 Asynchronous Programming/01.GITHUB-COMMITS/app.js
+++ 05 Asynchronous Programming/01.GITHUB-COMMITS/app.js
@@ -1,3 +1,28 @@
 function loadCommits() {
+    document.querySelector('#commits').innerHTML = '';
 
+    const userName = document.querySelector('#username').value;
+    const repository = document.querySelector('#repo').value;
+    const url = `https://api.github.com/repos/${userName}/${repository}/commits`;
+
+    fetch(url)
+        .then((response) => {
+            if (response.status >= 300) {
+                throw new Error(`${response.status} (${response.statusText})`);
+            }
+
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach(commitObj => {
+                const authorName = commitObj.commit.author.name;
+                const commitMessage = commitObj.commit.message;
+
+                const liElement = `<li>${authorName}: ${commitMessage}</li>`;
+                document.querySelector('#commits').innerHTML += liElement;
+            });
+        })
+        .catch((error) => {
+            document.querySelector('#commits').innerHTML = '<li>' + error.toString() + '</li>';
+        });
 }
