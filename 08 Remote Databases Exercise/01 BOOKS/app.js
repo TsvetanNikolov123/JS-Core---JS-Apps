c54b51b90382ca255d90ae3bf57fdee3c6069747
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-07-24 23:54:02 +0300

    solution - 01.Books

--- 08 Remote Databases Exercise/01 BOOKS/app.js
+++ 08 Remote Databases Exercise/01 BOOKS/app.js
@@ -22,8 +22,8 @@ elements.tbodyBooks.innerHTML = '';
 
 elements.btnSubmit.addEventListener('click', addBook);
 elements.btnLoadBooks.addEventListener('click', loadBooks);
-// elements.btnDoneEdit.addEventListener('click', editBook);
-// elements.btnCancelEdit.addEventListener('click', cancelEdit);
+elements.btnDoneEdit.addEventListener('click', editBook);
+elements.btnCancelEdit.addEventListener('click', cancelEdit);
 
 function addBook(ev) {
     ev.preventDefault();
@@ -58,7 +58,6 @@ function addBook(ev) {
 
 function loadBooks() {
     const headers = {
-        'Content-type': 'application/json',
         'Authorization': 'Basic ' + GET_BASE_64
     };
 
@@ -91,17 +90,98 @@ function loadBooks() {
                     .addEventListener('click', () => deleteBook(book._id));
 
                 elements.tbodyBooks.appendChild(trNextBook);
+                clearElementsValue(elements.inputTitle, elements.inputAuthor, elements.inputIsbn);
             });
+
         })
         .catch(err => console.log(err));
+
+    fromEditToSubmitForm();
 }
 
-function cancelEdit() {
+function fromEditToSubmitForm() {
+    clearElementsValue(elements.inputTitle, elements.inputAuthor, elements.inputIsbn);
+    elements.h3Form.textContent = 'FORM';
+    elements.btnSubmit.style.display = 'block';
+    elements.btnDoneEdit.style.display = 'none';
+    elements.btnCancelEdit.style.display = 'none';
 
 }
 
-function editBook() {
+function clearElementsValue(...arguments) {
+    arguments.forEach(element =>{
+        element.value = '';
+    })
+}
 
+function cancelEdit(ev) {
+    ev.preventDefault();
+    fromEditToSubmitForm();
+}
+
+function editBook(ev) {
+    ev.preventDefault();
+
+    const bookId = ev.target.value;
+    ev.target.value = '';
+
+    const bookData = {
+        'title': elements.inputTitle.value,
+        'author': elements.inputAuthor.value,
+        'isbn': elements.inputIsbn.value,
+    };
+
+    const editUrl = `${BASE_URL}/${bookId}`;
+    const headers = {
+        method: 'PUT',
+        body: JSON.stringify(bookData),
+        credentials: 'include',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Basic ' + GET_BASE_64
+        }
+    };
+
+    fetch(editUrl, headers)
+        .then(handler)
+        .then(loadBooks)
+        .catch(err => console.log(err))
+}
+
+function deleteBook(bookId) {
+    const deleteUrl = `${BASE_URL}/${bookId}`;
+
+    const headers = {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Basic ' + GET_BASE_64
+        }
+    };
+
+    fetch(deleteUrl, headers)
+        .then(handler)
+        .then(loadBooks)
+        .then(err => console.log(err));
+}
+
+function loadEditForm(bookId) {
+    const dataToEdit = document
+        .getElementById(bookId)
+        .querySelectorAll('td');
+
+    elements.inputTitle.value = dataToEdit[0].textContent;
+    elements.inputAuthor.value = dataToEdit[1].textContent;
+    elements.inputIsbn.value = dataToEdit[2].textContent;
+
+    elements.h3Form.textContent = 'Edit Book';
+
+    elements.btnSubmit.style.display = 'none';
+    elements.btnDoneEdit.style.display = 'block';
+    elements.btnCancelEdit.style.display = 'block';
+
+    elements.btnDoneEdit.value = bookId;
 }
 
 function handler(response) {
